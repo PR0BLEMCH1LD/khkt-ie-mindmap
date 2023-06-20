a2f38e0f2b1b9be9a605df942aff7afeb92903a1
Alexander E. Dang <alex.dang3606@gmail.com>
2023-06-20 18:09:11 +0700

    refactor sumshit

--- src/gfx/camera.c
+++ src/gfx/camera.c
@@ -7,24 +7,28 @@ void camera_init(Camera *self, f32 fov, f32 aspect_ratio, f32 znear, f32 zfar) {
 }
 
 void camera_tick(Camera *self) {
+	if (!state.window.mouse.buttons[GLFW_MOUSE_BUTTON_2].down) {
+		return;
+	}
+
 	vec3s forward = (vec3s){ { cosf(self->yaw), 0.0f, sinf(self->yaw) } };
 	vec3s up = (vec3s){ { 0.0f, 1.0f, 0.0f } };
 	vec3s right = glms_vec3_cross(forward, up);
-	vec3s hor_movement, ver_movement, movement;
+	vec3s hor_movement = glms_vec3_scale(right, -state.window.mouse.delta.x);
+	vec3s ver_movement;
 
-	if (state.window.mouse.buttons[GLFW_MOUSE_BUTTON_2].down) {
-		if (!state.window.mouse.buttons[GLFW_MOUSE_BUTTON_1].down) {
-			hor_movement = glms_vec3_scale(right, -state.window.mouse.delta.x);
-			ver_movement = glms_vec3_scale(forward, state.window.mouse.delta.y);
-		}
-		else {
-			hor_movement = glms_vec3_scale(right, -state.window.mouse.delta.x);
-			ver_movement = glms_vec3_scale(up, state.window.mouse.delta.y);
-		}
-
-		movement = glms_vec3_scale(glms_vec3_add(hor_movement, ver_movement), self->speed / state.world.tickrate);
-		self->position = glms_vec3_add(self->position, movement);
+	if (!state.window.mouse.buttons[GLFW_MOUSE_BUTTON_1].down) {
+		ver_movement = glms_vec3_scale(forward, state.window.mouse.delta.y);
 	}
+	else {
+		ver_movement = glms_vec3_scale(up, state.window.mouse.delta.y);
+	}
+
+	vec3s movement = glms_vec3_scale(
+						glms_vec3_add(hor_movement, ver_movement),
+						self->speed / state.world.tickrate
+					 );
+	self->position = glms_vec3_add(self->position, movement);
 }
 
 void camera_update(Camera *self) {
